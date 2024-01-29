af5f653de7f3c165c47226237849fcfc51cd7747
Shreyas911 <shreyasg911@gmail.com>
2024-01-29 03:34:21 -0600

    For OI, the gradient is for a loss function now instead of a jacobian for model output. A nice example highlighting the use of this gradient to get OI is also added to the jupyter notebook.

--- src/XAIRT/__init__.py
+++ src/XAIRT/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from XAIRT.backend.graph import getLayerIndexByName
+from XAIRT.backend.graph import getLayerIndexByName, useGradientTape, tf_to_numpy
 from XAIRT.model.Trainer import TrainLR, TrainFullyConnectedNN
 from XAIRT.model.XAI import XLR, XAIR
 from XAIRT.utils.stats import correlation
--- src/XAIRT/backend/graph.py
+++ src/XAIRT/backend/graph.py
@@ -2,14 +2,18 @@
 from __future__ import annotations
 
 import warnings
+import numpy as np
 warnings.simplefilter("ignore")
 
+from XAIRT.backend.types import Tensor, Callable, TensorNumpy
+
 import tensorflow.keras as keras
+import tensorflow as tf
 
 # Get useful types from types.py
 from XAIRT.backend.types import kModel
 
-__all__ =["getLayerIndexByName"]
+__all__ =["getLayerIndexByName", "useGradientTape", "tf_to_numpy"]
 
 def getLayerIndexByName(model: kModel, layername: str) -> int:
 	for idx, layer in enumerate(model.layers):
@@ -17,4 +21,25 @@ def getLayerIndexByName(model: kModel, layername: str) -> int:
 			return idx
 	raise ValueError(f"layername: {layername} not found.")
 
-	
+# Useful function for Optimal Input calculations
+@tf.function # Crucial function decorator for speedup
+def useGradientTape(model: kModel, 
+					x: Tensor,
+					desired_labels: Tensor,
+					compute_loss: Callable) -> Tensor:
+
+    with tf.GradientTape(persistent=True) as g:
+        g.watch(x)
+        preds = model(x)
+        loss = compute_loss(desired_labels, preds)
+        # Expand dimensions of scalar loss to (1,1)
+        loss = tf.expand_dims(tf.expand_dims(loss, axis=(0,)), axis=(0,))
+    
+    # This has to be outside the with statement for efficiency, unless you want higher order derivatives.
+    grads = g.batch_jacobian(loss, x)
+
+    return grads
+
+# Tf to Numpy when eager execution is disabled, which is the case for LRP.
+def tf_to_numpy(x: Tensor) -> TensorNumpy:
+    return np.array(tf.keras.backend.get_value(x))
