45fb7b429b8eccdf0e905f124c106a6a86496dbb
Shreyas911 <shreyasg911@gmail.com>
2024-01-30 05:25:15 -0600

    useGradientTape not working for keras optimizers, so commented out. Sticking with vanilla gradient descent for OI.

--- src/XAIRT/backend/graph.py
+++ src/XAIRT/backend/graph.py
@@ -40,29 +40,30 @@ def GradientDescent_useGradientTape(model: kModel,
 
     return grads
 
-### NOTE: Requires eager execution to be ENABLED
-@tf.function # Crucial function decorator for speedup
-def TrainOI_useGradientTape(model: kModel,
-					  x: Variable,
-					  desired_labels: Variable,
-					  compute_loss: Callable,
-                      optimizer: Optimizer) -> None:
+### NOT WORKING AS INTENDED SO FAR
+# ### NOTE: Requires eager execution to be ENABLED
+# @tf.function # Crucial function decorator for speedup
+# def TrainOI_useGradientTape(model: kModel,
+# 					  x: Variable,
+# 					  desired_labels: Variable,
+# 					  compute_loss: Callable,
+#                       optimizer: Optimizer) -> None:
 
-    # Because apply_gradient takes list of (gradient, variable) pairs, 
-    # input must be a instance of tf.Variable rather than tf.Tensor.
-    # https://github.com/tensorflow/tensorflow/issues/31273
-    # https://colab.research.google.com/github/tensorflow/docs/blob/snapshot-keras/site/en/guide/keras/writing_a_training_loop_from_scratch.ipynb
-    with tf.GradientTape() as g:
-        g.watch(x)
-        preds = model(x)
-        loss = compute_loss(desired_labels, preds)
+#     # Because apply_gradient takes list of (gradient, variable) pairs, 
+#     # input must be a instance of tf.Variable rather than tf.Tensor.
+#     # https://github.com/tensorflow/tensorflow/issues/31273
+#     # https://colab.research.google.com/github/tensorflow/docs/blob/snapshot-keras/site/en/guide/keras/writing_a_training_loop_from_scratch.ipynb
+#     with tf.GradientTape() as g:
+#         g.watch(x)
+#         preds = model(x)
+#         loss = compute_loss(desired_labels, preds)
 
-    # This has to be outside the with statement for efficiency, unless you want higher order derivatives.
-    grads = g.gradient(loss, x)
-    # Apply gradient using in-built tf optimizer
-    optimizer.apply_gradients(zip([grads], [x]))
+#     # This has to be outside the with statement for efficiency, unless you want higher order derivatives.
+#     grads = g.gradient(loss, x)
+#     # Apply gradient using in-built tf optimizer
+#     optimizer.apply_gradients(zip([grads], [x]))
 
-    return loss
+#     return loss
 
 # Tf to Numpy when eager execution is disabled, which is the case for LRP.
 def tf_to_numpy(x: Tensor) -> TensorNumpy:
